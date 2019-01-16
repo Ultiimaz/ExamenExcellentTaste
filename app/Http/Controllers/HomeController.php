6df653a2aedcf5284579e2ab5c2e8ef1ee360d6c
LAPTOP-G3F8C8DJ\Jordie <info@jordiepasman.nl>
2019-01-16 09:41:31 +0100

    Taak-23: foreign keys model Reservation:Customers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Reservation;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+
+        //$phone = Reservation::find(12345678)->customer();
+        //dd($phone);
+
         return view('home');
+
     }
 }
