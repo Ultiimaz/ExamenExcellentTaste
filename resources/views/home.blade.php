5e8f2da4503d9f67f80fb7f49e70042ccbd16933
LAPTOP-G3F8C8DJ\Jordie <info@jordiepasman.nl>
2019-01-21 12:41:43 +0100

    Home.blade aanpassen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,49 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
-@section('test')
+@section('page')
+    <style>
+        #map {
+            width: 100%;
+            height: 350px;
+            background-color: grey;
+        }
+    </style>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-md-flex align-items-center">
+                        <div>
+                            <h3 class="card-title">Home</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- column -->
+                        <div class="col-lg-12">
+                            Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen.
+                            Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
+                            toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een
+                            font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd,
+                            overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van
+                            Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.
+
+                        </div>
+                        <!-- column -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title"></h4>
+
+                    <im
+
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 @endsection
