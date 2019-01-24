dcf1cadf34b9e75437974efc3ef7935bf2c7ef22
Ruben Veerman <rubjeveerman@gmail.com>
2019-01-24 09:44:49 +0100

    menukaart laten zien

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rubje
+ * Date: 24-Jan-19
+ * Time: 09:26
+ */
+
+namespace App\Http\Controllers;
+
+
+class MenuController
+{
+
+}
---
+++ database/seeds/MenuSeeder.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rubje
+ * Date: 24-Jan-19
+ * Time: 09:12
+ */
+
+class MenuSeeder
+{
+
+}
