eba7688c31494e9618ed499b4c091322caf6d8ec
kasper <kasper_vegte@hotmail.com>
2019-01-25 09:13:58 +0100

    Fixed registration verification aswell as cleaned up routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,11 @@
     </style>
     <div class="row">
         <div class="col-md-8">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
             <div class="card">
                 <div class="card-body">
                     <div class="d-md-flex align-items-center">
